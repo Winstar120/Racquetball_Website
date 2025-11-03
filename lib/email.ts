6d6d62281922c44049dfcdb177f2240cfc324027
Winstar120 <winstar120@github.com>
2025-11-03 11:59:12 -0700

    more fixes

--- lib/email.ts
+++ lib/email.ts
@@ -1,13 +1,15 @@
 import nodemailer from 'nodemailer';
 import {
-  EmailType,
-  EmailStatus,
+  $Enums,
   type User,
   type Match,
   type League,
   type Division,
 } from '@prisma/client';
 import { prisma } from '@/lib/prisma';
+
+const EmailType = $Enums.EmailType;
+const EmailStatus = $Enums.EmailStatus;
 
 // Create reusable transporter
 const transporter = process.env.SMTP_USER && process.env.SMTP_PASSWORD
